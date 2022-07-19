9bdaa5ce598878b14fb666414a97a043b5f6021b
Alexander Miryasov <alex.miryasov99@yandex.ru>
2022-07-19 13:05:15 +0600

    Fixed AndroidManifest exports

--- react-native.config.js
+++ react-native.config.js
@@ -6,8 +6,8 @@ module.exports = {
             }
         },
         hooks: {
-            postlink: "node ./node_modules/@darron1217/react-native-background-geolocation/scripts/postlink.js",
-            postunlink: "node ./node_modules/@darron1217/react-native-background-geolocation/scripts/postunlink.js"
+            postlink: "node ./node_modules/@thekingzoom/react-native-background-geolocation/scripts/postlink.js",
+            postunlink: "node ./node_modules/@thekingzoom/react-native-background-geolocation/scripts/postunlink.js"
         }
     }
 };
